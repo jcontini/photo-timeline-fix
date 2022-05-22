fd105e66d9ba4554597dff5de38eb99c4a2437f9
Joe C <joe@contini.co>
2022-05-22 10:54:56 +0800

    Support for modifying file dates

--- run.py
+++ run.py
@@ -6,16 +6,19 @@ log = logging.info
 con = sqlite3.connect('updates.db')
 cur = con.cursor()
 
-TOP_FOLDER = '/Users/Joe/Downloads/lisa'
+TOP_FOLDER = '/Users/Joe/Downloads/lisa/'
 
 def init_db():
-    '''Init Database tables'''
-    cur.execute('''
-    CREATE TABLE folders (
-        path TEXT PRIMARY KEY, 
-        year INT
-    )''')
-    con.close()
+    try:
+        '''Init Database tables'''
+        cur.execute('''
+        CREATE TABLE folders (
+            path TEXT PRIMARY KEY, 
+            year INT
+        )''')
+        print("Database initilized")
+    except sqlite3.OperationalError:
+        print("Database already initialized")
 
 def prep(folder,newyear):
     '''Add folder & year to folders table'''
@@ -46,48 +49,52 @@ def index(folder):
                 new_year = subfolder_year
                 prep(subfolder,new_year)'''
 
+    print("Database updated")
+    
+    con.commit()
+    con.close()
+
 def make_updates():
     '''Update files with new modified date & EXIF data'''
     folders = cur.execute("SELECT * FROM folders")
     for folder in folders:
-        print(folder[0])
+        folder_path = TOP_FOLDER + folder[0] + '/'
+        print(folder_path)
 
-        for file in folder:
+        files = next(os.walk(folder_path))[2]
+        for filename in files:
+            file_path = folder_path + filename
+            print(file_path)
+
             # Prep new dateime based on year
             new_year = int(folder[1])
-            print(new_year)
-            new_dt = datetime.datetime(new_year, 1, 1)
+            naive_dt = datetime.datetime(new_year, 1, 1,12,11,11)
+            new_dt = naive_dt.replace(tzinfo=datetime.timezone.utc)
+            new_ts = new_dt.timestamp()
 
             # Update modified date (for all files/media, inc videos)
-            ## TODO change to integer
-            os.utime(file, (new_dt,)*2)
-            log("[%s] Modified date: %s" % (file, new_dt))
+            os.utime(file_path, (new_ts,)*2)
+            log(f"{file_path} Modified date: {new_ts}")
 
             try:
-                # Update Exif metadata (for supported images)
-                exif_dict = piexif.load(file)
-                if piexif.ExifIFD.DateTimeOriginal not in exif_dict['Exif']:
-                    exif_date = time.strftime("%Y:%m:%d %H:%M:%S", time.localtime(new_dt))
-                    exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = exif_date
-                    piexif.insert(piexif.dump(exif_dict), file)
+                # Update Exif metadata (for supported images eg JPEG/WebP)
+                exif_dict = piexif.load(file_path)
+                exif_date = time.strftime("%Y:%m:%d %H:%M:%S", new_dt)
+                exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = exif_date
+                piexif.insert(piexif.dump(exif_dict), file_path)
 
-            except:
-                log("[%s] Couldn't update EXIF" % file)
+            except TypeError: #update error type
+                log(f"[{file_path}] Couldn't update EXIF")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Photo year updater')
-    parser.add_argument('--init', action='store_true', help='Initialize DB')
     parser.add_argument('--index', action='store_true', help='Prep updates in db')
     parser.add_argument('--write', action='store_true', help='Write updates to files')
     args = parser.parse_args()
 
-    if args.init:
-        init_db()
-
     if args.index:
+        init_db()
         index(TOP_FOLDER)
-        con.commit()
-        con.close()
-
+        
     if args.write:
         make_updates()
